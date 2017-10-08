97aa4cfeb5bc7241a7fbd455bbfb9edfb1681284
Amro Diab <adiab@hotmail.co.uk>
2017-10-08 19:50:07 +0100

    parse temperature data

--- temperature.py
+++ temperature.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python2
-
+"""
+http://tinyurl.com/filtest
+"""
+import json
 import os
+import urllib2
 
-from flask import Flask, request, abort, Response
+from flask import Flask
 
 HOME = os.getenv("HOME")
-APIKEY = open(HOME + "/.weatherkey", "r").readlines()[0].strip()
+API_KEY = open(HOME + "/.weatherkey", "r").readlines()[0].strip()
+URL = ('http://api.worldweatheronline.com/premium/v1/weather.ashx?key='
+       '{0}&q=London&format=json&num_of_days=3&tp=1'.format(API_KEY))
+TEMP_THRESHOLD = 15
 
-# FIXME:
-#os.system("curl 'http://api.worldweatheronline.com/premium/v1/weather.ashx?key={0}&q=London&format=json&num_of_days=3&tp=1'".format(APIKEY))
+def get_data(url):
+    """ Fetch data from external API """
+    data = urllib2.urlopen(url).read()
+    json_data = json.loads(data)
+    return json_data
+
+JSON = get_data(URL)
+#print JSON['data'].keys()
+
 
 FLASK_APP = Flask(__name__)
 
@@ -17,7 +31,24 @@ def time_to_heatwave():
     return '{"hours_until_heatwave": "36" }'
 
 def main():
-    FLASK_APP.run(debug=True, threaded=False)
+    overall_hours = 0    # keep count of number of hours accross days
+    for day in range(0, 3):  # iterate through days
+        for hour in range(0, 23,):  # iterate through hours in a day
+            overall_hours += 1  # increment overall hours
+            # get temp for current hour
+            temp = int(get_data(URL)['data']['weather'][day]['hourly'][hour]['tempC'])
+            if temp > TEMP_THRESHOLD:
+                hours_until_heatwave = overall_hours
+                break  # Break out of loop as we have crossed the temp threshold
+        else:
+            continue  # contonue loop
+        break  # break out of day threshold
+
+    print "AMROX " + str(hours_until_heatwave)
+    print len(get_data(URL)['data']['weather'][0]['hourly']) #[day]['maxtempC']
+    #for day in range(0, 3):
+      #print get_data(URL)['data']['weather'] #[day]['maxtempC']
+    #FLASK_APP.run(debug=True, threaded=False)
     #get_heatwave_time()
 
 if __name__ == '__main__':
