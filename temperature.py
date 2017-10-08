e04e0b5d2f5f00caa059bc991471faa22ea48c5c
Amro Diab <adiab@hotmail.co.uk>
2017-10-08 21:34:20 +0100

    cleanup & fix lint

--- temperature.py
+++ temperature.py
@@ -1,40 +1,53 @@
 #!/usr/bin/env python2
+#pylint: disable=global-statement
+
 """
 http://tinyurl.com/filtest
 """
+
 import json
 import os
 import sched
+import sys
+import threading
 import time
-import calendar
 import urllib2
 
-import threading
 from flask import Flask
 
 DATA = None
 HOME = os.getenv("HOME")
+
+# Get API key from file if it exists, otherwise default to env var
 try:
     API_KEY = open(HOME + "/.weatherkey", "r").readlines()[0].strip()
-except (FileNotFoundError, PermissionError):
+except IOError:
     API_KEY = os.getenv("API_KEY")
+
 if API_KEY is None:
-   sys.exit('API Key not found, unable to proceed')
+    sys.exit('API Key not found, unable to proceed')
 
 URL = ('http://api.worldweatheronline.com/premium/v1/weather.ashx?key='
        '{0}&q=London&format=json&num_of_days=3&tp=1'.format(API_KEY))
 TEMP_THRESHOLD = 27
 TIMER = 3600
-s = sched.scheduler(time.time, time.sleep)
+FLASK_APP = Flask(__name__)
+SCHED = sched.scheduler(time.time, time.sleep)
 
-def schedule_data(sc):
-    print "scheduling"
-    print threading.current_thread()
+def schedule_data(scheduler):
+    """
+    Scheduler function for fetching data hourly
+    Runs in a background thread to not interfere with flask
+    """
+
     global DATA
-    DATA = get_data(URL)
-    s.enter(3600, 1, schedule_data,(sc,))
+    try:
+        DATA = get_data(URL)
+        sys.stdout.write("Successfullly fetched data")
+    except urllib2.URLError:
+        sys.stderr.write("Error opening URL\n")
 
-
+    SCHED.enter(TIMER, 1, schedule_data, (scheduler,))
 
 def get_data(url):
     """ Fetch data from external API """
@@ -42,25 +55,38 @@ def get_data(url):
     json_data = json.loads(data)
     return json_data
 
-JSON = get_data(URL)
-#print JSON['data'].keys()
 
-
-FLASK_APP = Flask(__name__)
 
 @FLASK_APP.route('/time_to_heatwave', methods=['GET'])
 def time_to_heatwave():
-    print calculate_time()
+    """
+    API endpoint to return number of hours to next heatwave as JSON
+    """
     return '{"hours_until_heatwave": %s}'% calculate_time()
 
 def main():
-    s.enter(0, 1, schedule_data, (s,))
-    background_thread = threading.Thread(target=s.run, args=())
+    """
+    Main function
+    Start scheduler to fetch data, and Flask API to serve data
+    """
+    # enter scheduler and fetch data immediately (0 seconds) for initial data
+    # Scheduler will handle future runs
+    SCHED.enter(0, 1, schedule_data, (SCHED,))
+    background_thread = threading.Thread(target=SCHED.run, args=())
     background_thread.start()
-    FLASK_APP.run(debug=True, threaded=False)
-    #SCHED.run()
+
+    # start Flask
+    FLASK_APP.run(debug=True, threaded=True)
 
 def calculate_time():
+    """
+    Calculate and return time to next heatwave if data is present
+    """
+
+    if DATA is None:
+        # If data has not yet been successfully fetched
+        return "No Data"
+
     hours_until_heatwave = "None"
     overall_hours = 0    # keep count of number of hours accross days
     for day in range(0, 3):  # iterate through days
@@ -75,13 +101,6 @@ def calculate_time():
             continue  # contonue loop
         break  # break out of day threshold
     return hours_until_heatwave
-
-    print "AMROX " + str(hours_until_heatwave)
-    print len(get_data(URL)['data']['weather'][0]['hourly']) #[day]['maxtempC']
-    #for day in range(0, 3):
-      #print get_data(URL)['data']['weather'] #[day]['maxtempC']
-    #FLASK_APP.run(debug=True, threaded=False)
-    #get_heatwave_time()
 
 if __name__ == '__main__':
     main()
